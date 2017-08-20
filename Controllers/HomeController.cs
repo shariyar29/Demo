ef6702832734413937aa9f72c0ea0b0ef814f523
shariyar29 <30766541+shariyar29@users.noreply.github.com>
2017-08-20 21:50:55 +0530

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Demo.Models;
+
+namespace Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        DemoEntities1 db = new DemoEntities1();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Employee obj)
+        {
+            if(!db.Employees.Any(x =>x.Username.Equals(obj.Username)))
+            { 
+            db.Employees.Add(obj);
+            db.SaveChanges();
+            ViewBag.msg = "Registered Sucessfully";//this will travel to this view 
+
+           
+            }
+            else
+            {
+                ViewBag.msg1 = "Already Registered";
+            }
+            ModelState.Clear();//Clears the data after submit is clicked
+            return View();
+
+        }
+        public ActionResult Display()
+        {
+            return View(db.Employees.ToList());
+        }
+        [HttpGet]
+        public ActionResult Edit(int?id)
+        {
+            Employee emp = db.Employees.Find(id);
+            return View(emp);
+        }
+        [HttpPost]
+        public ActionResult Edit(Employee e)
+        {
+            Employee evalue = db.Employees.Find(e.EID);
+            evalue.Ename = e.Ename;
+            evalue.Password = e.Password;
+            evalue.EDOJ = e.EDOJ;
+            evalue.Edesignation = e.Edesignation;
+            evalue.Edept = e.Edept;
+            evalue.Ecity = e.Ecity;
+            db.SaveChanges();
+            ViewBag.Success = "Update Successfull";
+            return View();
+        }
+    }
+}
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Demo.Models;
+
+namespace Demo.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        DemoEntities1 db = new DemoEntities1();
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Employee obj)
+        {
+            var ob = db.Employees.Where(a => a.Username.Equals(obj.Username) && a.Password.Equals(obj.Password)).FirstOrDefault();
+            if(ob !=null)
+            {
+                return RedirectToAction("Display", "Home");
+            }
+            ViewBag.msg = "Invalied username or password";
+            return View();
+        }
+    }
+}
